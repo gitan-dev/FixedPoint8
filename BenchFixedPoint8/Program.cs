667faa15034f22bfd0e32e70c02fbface693029a
mewroar <131640209+mewroar@users.noreply.github.com>
2023-11-25 14:23:01 +0900

    Gitan FixedPoint8 1.0.1
    changed the target framework.

--- BenchFixedPoint8/Program.cs
+++ BenchFixedPoint8/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace Gitan.FixedPoint8;
+﻿using BenchmarkDotNet.Running;
+
+namespace Gitan.FixedPoint8;
 
 public class Program
 {
+    //static void Main(string[] args)
+    //{
+    //    BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    //}
+
     public static void Main()
     {
-        //BenchmarkDotNet.Running.BenchmarkRunner.Run<BenchMark_Calc>();
+        BenchmarkDotNet.Running.BenchmarkRunner.Run<BenchMark_Calc>();
         BenchmarkDotNet.Running.BenchmarkRunner.Run<BenchMark_Characters>();
-        //BenchmarkDotNet.Running.BenchmarkRunner.Run<BenchMark_Utf8Json>();
+        BenchmarkDotNet.Running.BenchmarkRunner.Run<BenchMark_Utf8Json>();
     }
 }
